5a8ce2b0f44005f2ad11b9c8ae5aedd7684af950
Monil Bhavsar <monilbhavsar25@gmail.com>
2020-03-15 21:55:58 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import os
+import zulip
+
+def main():
+    client = zulip.Client(config_file="~/zuliprc")
+	# Send a private message
+	request = {
+	    "type": "private",
+	    "to": ['monilbhavsar.england99@gmail.com'],
+	    "content": "Hello!!"
+	}
+	result = client.send_message(request)
+	print(result)
+
+if __name__ == "__main__":
+    main()
