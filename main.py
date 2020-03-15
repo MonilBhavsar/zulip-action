3346f19b6d07e961c76caad0d2003904d851ca8a
Monil Bhavsar <monilbhavsar25@gmail.com>
2020-03-15 22:25:34 +0530

    changes

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 import requests
 import os
 import zulip
 
-def main():
-    client = zulip.Client(config_file="~/zuliprc")
-	# Send a private message
-	request = { "type": "private","to": ['monilbhavsar.england99@gmail.com'],"content": "Hello" }
-	result = client.send_message(request)
-	print(result)
 
-if __name__ == "__main__":
+def main():
+    client = zulip.Client(config_file='~/zuliprc')
+
+    # Send a private message
+
+    request = {'type': 'private',
+               'to': ['monilbhavsar.england99@gmail.com'],
+               'content': 'Hello'}
+    result = client.send_message(request)
+    print result
+
+
+if __name__ == '__main__':
     main()
