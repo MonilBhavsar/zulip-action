d31c46025d762a7aa8fa37193a7120309498d8b9
Monil Bhavsar <monilbhavsar25@gmail.com>
2020-03-15 22:11:30 +0530

    changes

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
     client = zulip.Client(config_file="~/zuliprc")
 	# Send a private message
 	request = {
-	    "type": "private",
-	    "to": ['monilbhavsar.england99@gmail.com'],
-	    "content": "Hello!!"
+		"type": "private",
+		"to": ['monilbhavsar.england99@gmail.com'],
+		"content": "Hello!!"
 	}
 	result = client.send_message(request)
 	print(result)
